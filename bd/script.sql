a7911dfcb80ff76b5e517e3683a109fb502487b9
Gustavo Salgado Lima <102550256+GusGul@users.noreply.github.com>
2022-10-20 00:40:50 -0300

    Restante das tabelas adicionadas

--- bd/script.sql
+++ bd/script.sql
@@ -5,7 +5,6 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
-
 CREATE SCHEMA IF NOT EXISTS quiz DEFAULT CHARACTER SET utf8 ;
 USE quiz ;
 
@@ -23,3 +22,33 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Criação da tabela de perguntas
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS quiz.tab_perguntas (
+    id_pergunta INT NOT NULL,
+    descricao VARCHAR(255) NULL,
+    PRIMARY KEY (id_pergunta)
+);
+
+-- -----------------------------------------------------
+-- Criação da tabela de perfis
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS quiz.tab_perfis (
+	  id_perfil INT NOT NULL,
+    nome VARCHAR(45) NULL,
+    descricao VARCHAR(255) NULL,
+    PRIMARY KEY (id_perfil)
+);
+
+-- -----------------------------------------------------
+-- Criação da tabela de respostas
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS quiz.tab_respostas (
+    id_resposta INT NOT NULL,
+    descricao VARCHAR(255) NULL,
+    id_perfil INT,
+    PRIMARY KEY (id_resposta),
+    CONSTRAINT FK_perfil FOREIGN KEY (id_perfil) REFERENCES quiz.tab_perfis(id_perfil) ON UPDATE CASCADE
+);
